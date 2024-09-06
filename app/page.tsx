91d5e52b7c8c008bf77d61cbe9b5af103c019237
ivanlie <ivan.lie@meditap.id>
2024-09-06 21:54:18 +0700

    revert code

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
-'use client'
-
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 import Content from './_components/content'
 import UserButton from './_components/user-button'
 
-export default function Home() {
-  // const { getUser } = getKindeServerSession()
-  // const user = await getUser()
-  console.log(process.env)
+export default async function Home() {
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+
   return (
     <main className="flex flex-col gap-6 h-full justify-center items-center p-6">
-      {/* <Content user={user} />
-      <UserButton user={user} /> */}
+      <Content user={user} />
+      <UserButton user={user} />
     </main>
   )
 }
