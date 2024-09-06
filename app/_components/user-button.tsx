69639ded8d800d433e185dab5da0206d48b08d85
ivanlie <ivan.lie@meditap.id>
2024-09-06 17:46:26 +0700

    remove unused code

--- app/_components/user-button.tsx
+++ app/_components/user-button.tsx
@@ -1,21 +1,16 @@
-'use client'
-
 import { LoginLink, LogoutLink } from '@kinde-oss/kinde-auth-nextjs/components'
 
 import { Button } from '@/components/ui/button'
-import { usePostHog } from 'posthog-js/react'
 
 interface Props {
   user: Record<string, any>
 }
 
 const UserButton = ({ user }: Props) => {
-  const posthog = usePostHog()
-
   return (
     <>
       {user ? (
-        <LogoutLink onClick={() => posthog.reset()}>
+        <LogoutLink>
           <Button>Logout</Button>
         </LogoutLink>
       ) : (
