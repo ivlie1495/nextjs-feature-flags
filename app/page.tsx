8dcc48091ac57cadd21e4d1bba8a044f24c531ae
ivanlie <ivan.lie@meditap.id>
2024-09-06 21:46:43 +0700

    log env

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
+'use client'
+
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 import Content from './_components/content'
 import UserButton from './_components/user-button'
 
-export default async function Home() {
-  const { getUser } = getKindeServerSession()
-  const user = await getUser()
-
+export default function Home() {
+  // const { getUser } = getKindeServerSession()
+  // const user = await getUser()
+  console.log(process.env)
   return (
     <main className="flex flex-col gap-6 h-full justify-center items-center p-6">
-      <Content user={user} />
-      <UserButton user={user} />
+      {/* <Content user={user} />
+      <UserButton user={user} /> */}
     </main>
   )
 }
