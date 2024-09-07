b4d54ef6fb89360218f5eb627dda0be8c637b77f
ivanlie <ivan.lie@meditap.id>
2024-09-07 22:13:40 +0700

    remove env from next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    KINDE_SITE_URL:
-      process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
-    KINDE_POST_LOGOUT_REDIRECT_URL:
-      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ??
-      `https://${process.env.VERCEL_URL}`,
-    KINDE_POST_LOGIN_REDIRECT_URL:
-      process.env.KINDE_POST_LOGIN_REDIRECT_URL ??
-      `https://${process.env.VERCEL_URL}/dashboard`,
-  },
-}
+const nextConfig = {}
 
 export default nextConfig
