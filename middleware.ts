07177efc4f213163cfb293ae61e8136d01732266
ivanlie <ivan.lie@meditap.id>
2024-09-09 11:21:54 +0700

    remove return to current page

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,6 @@ import { withAuth } from '@kinde-oss/kinde-auth-nextjs/middleware'
 
 export default withAuth({
   loginPage: '/api/auth/login',
-  isReturnToCurrentPage: true,
 })
 
 export const config = {
