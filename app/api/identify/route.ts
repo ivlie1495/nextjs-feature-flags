95e3fd5b8bed5c267f3676151d6bfc82ff960ad5
ivanlie <ivan.lie@meditap.id>
2024-09-09 10:02:15 +0700

    add api identify

---
+++ app/api/identify/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+
+import posthogClient from '@/utils/posthog'
+
+export const GET = async () => {
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+
+  posthogClient.identify({
+    distinctId: user.id,
+    properties: {
+      email: user.email,
+    },
+  })
+
+  return NextResponse.redirect('/')
+}
